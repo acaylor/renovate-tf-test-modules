afc8f82b83ce208ebbb4f7f89c6c0ac765a1dc98
acaylor <acaylor@redoxengine.com>
2024-07-02 11:20:54 -0600

    add second module and branch prefix config

---
+++ modules/test-module2/main.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_role" "test_role2" {
+  name = "test-role2"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "s3.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    foo = "bar"
+    bar = "foo"
+  }
+
+}
